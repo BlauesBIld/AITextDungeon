818720c6e18264d0a857de53228d4ee8f6a79ed9
Detschn <de.zeljko@gmx.at>
2023-11-18 19:21:36 +0100

    Added Bottom Navigation Bar.

--- app/src/main/java/zeljko/dejan/rpginventorymanager/AddingProcessActivity.kt
+++ app/src/main/java/zeljko/dejan/rpginventorymanager/AddingProcessActivity.kt
@@ -1,11 +1,54 @@
 package zeljko.dejan.rpginventorymanager
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
+import zeljko.dejan.rpginventorymanager.databinding.ActivityAddingProcessBinding
 
 class AddingProcessActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityAddingProcessBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_adding_process)
+        binding = ActivityAddingProcessBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupNavigationBar()
+        showSetNameScreen() // Initialize with the first screen
+    }
+
+    private fun setupNavigationBar() {
+        binding.navigationBar.setOnItemSelectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_name -> showSetNameScreen()
+                R.id.navigation_icon -> showSelectIconScreen()
+                R.id.navigation_properties -> showSetPropertiesScreen()
+                R.id.navigation_category -> showAssignCategoryScreen()
+            }
+            true
+        }
+    }
+
+    private fun showSetNameScreen() {
+        switchToScreen(R.layout.new_item_set_name)
+    }
+
+    private fun showSelectIconScreen() {
+        switchToScreen(R.layout.new_item_select_icon)
+    }
+    private fun showSetPropertiesScreen() {
+        switchToScreen(R.layout.new_item_set_properties)
+    }
+
+    private fun showAssignCategoryScreen() {
+        switchToScreen(R.layout.new_item_assign_category)
+    }
+
+    private fun switchToScreen(newScreen: Int) {
+        val inflater = LayoutInflater.from(this)
+        val view = inflater.inflate(newScreen, binding.contentFrame, false)
+        binding.contentFrame.removeAllViews()
+        binding.contentFrame.addView(view)
     }
 }
